ec5b72c449206dabe41bf79c06f76c67f4dc23a0
Yuan Haochao <gregoryyuan160@gmail.com>
2023-03-09 17:37:08 +0100

    initial Commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Bonjour,\n" +
+                "quel est votre prénom ?");
+
+    }
+
+    }
